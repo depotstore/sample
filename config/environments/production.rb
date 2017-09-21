1ceac64b128c18807b7abe9c6a43e194e0a94887
Serge <sergcde.gmail.com>
2017-09-21 22:14:04 +0300

    Correct email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,37 +64,37 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = true
-  # config.action_mailer.delivery_method = :smtp
-  # host = 'example-sample-application.herokuapp.com'
-  # config.action_mailer.default_url_options = { host: host }
-  # ActionMailer::Base.smtp_settings = {
-  #   address: 'smtp.sendgrid.net',
-  #   port: '587',
-  #   authentication: :plain,
-  #   user_name: ENV['app74687392@heroku.com'],
-  #   password:  ENV['9k6emvuv6406'],
-  #   domain: 'heroku.com',
-  #   enable_starttls_auto: true
-  # }
-
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = true
-  config.action_mailer.perform_deliveries = true
-  #Sending emails
-  config.action_mailer.delivery_method =   :smtp #:test
-  config.action_mailer.default_url_options = {host: '46.188.127.158:80'}
-  config.action_mailer.smtp_settings = {
-    address:        "smtp.gmail.com",
-    port:           587,
-    domain:         "46.188.127.158:80", #"domain.of.sender.net" gmail.com
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'example-sample-application.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
     authentication: :plain,
-    user_name:      'depotstoreinfo@gmail.com',
-    password:       'Store123',
+    user_name: ENV['app77413761@heroku.com'],
+    password:  ENV['vwnueb374424'],
+    domain: 'heroku.com',
     enable_starttls_auto: true
   }
+
+  # # Don't care if the mailer can't send.
+  # config.action_mailer.raise_delivery_errors = false
+  #
+  # config.action_mailer.perform_caching = true
+  # config.action_mailer.perform_deliveries = true
+  # #Sending emails
+  # config.action_mailer.delivery_method =   :smtp #:test
+  # config.action_mailer.default_url_options = {host: '46.188.127.158:80'}
+  # config.action_mailer.smtp_settings = {
+  #   address:        "smtp.gmail.com",
+  #   port:           587,
+  #   domain:         "46.188.127.158:80", #"domain.of.sender.net" gmail.com
+  #   authentication: :plain,
+  #   user_name:      'depotstoreinfo@gmail.com',
+  #   password:       'Store123',
+  #   enable_starttls_auto: true
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
